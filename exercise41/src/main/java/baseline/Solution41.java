7b1acc23d9e4962dd6def66de794cb0ae277f00d
Simon <simonfurry@gmail,.com>
2021-10-14 22:25:21 -0400

    Skeleton of code exercise41

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,21 @@
+package baseline;
+/*
+ * UCF COP 3330 Fall 2021 Assignment 4 Solutions
+ * Copyright 2021 Simon Furry
+ */
+
+public class Solution41 {
+    /*
+    Organize the contents of a text file alphebetically
+    take a list of names from a file named exercise41_input.txt
+    count total number of names and organize them alphebetically by last name
+     */
+
+    public static void main(String[] args){
+        organizeNames();
+    }
+
+    public static void organizeNames(){
+        
+    }
+}
