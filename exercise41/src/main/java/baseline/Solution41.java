9f91b6f8a57cca63539b62d5ef401871d82a9f17
Simon <simonfurry@gmail,.com>
2021-10-14 22:54:39 -0400

    Commit of exercise41 logic

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -4,18 +4,43 @@ package baseline;
  * Copyright 2021 Simon Furry
  */
 
+import java.io.*;
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Solution41 {
     /*
-    Organize the contents of a text file alphebetically
+    Organize the contents of a text file alphabetically
     take a list of names from a file named exercise41_input.txt
-    count total number of names and organize them alphebetically by last name
+    count total number of names and organize them alphabetically by last name
      */
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         organizeNames();
     }
 
-    public static void organizeNames(){
-        
+    public static void organizeNames() throws IOException {
+        FileReader fileReader = new FileReader("exercise41_input.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        List<String> lineCount = new ArrayList<>();
+
+        String line = null;
+        while((line = bufferedReader.readLine()) != null){
+            lineCount.add(line);
+        }
+        bufferedReader.close();
+
+            Collections.sort(lineCount, Collator.getInstance());
+
+            FileWriter writer = new FileWriter("exercise41_output.txt");
+            writer.write("Total of " + lineCount.size() + " names\n---------------------\n");
+            for(String str:lineCount){
+                writer.write(str + "\r\n");
+            }
+            writer.close();
     }
 }
