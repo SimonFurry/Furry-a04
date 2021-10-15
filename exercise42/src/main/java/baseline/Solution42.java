ff8d71e36fd98ffdf75a82dd5b63f7cd8037b39d
Simon <simonfurry@gmail,.com>
2021-10-14 23:13:33 -0400

    Skeleton code of exercise42:

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,16 @@
+package baseline;
+/*
+ * UCF COP 3330 Fall 2021 Assignment 4 Solutions
+ * Copyright 2021 Simon Furry
+ */
+
+public class Solution42 {
+    /*
+    Read "exercise42_input.txt"
+    process the records and display the results formatted as a table
+    evenly spaced
+    Last, First, Salary
+    String[] splitter = scanner.nextLine().split(",");
+    reads data and breaks it at a comma, making a CSV from scratch
+     */
+}
