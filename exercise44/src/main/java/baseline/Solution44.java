a0145ff41302cc5716a29d2754663c51db839e8d
Simon <simonfurry@gmail,.com>
2021-10-16 14:11:22 -0400

    Skeleton of exercise44

---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,21 @@
+package baseline;
+/*
+ * UCF COP 3330 Fall 2021 Assignment 4 Solutions
+ * Copyright 2021 Simon Furry
+ */
+
+public class Solution44 {
+    /*
+    Create a program that takes a product name as input
+   and retrieves the current price and quantity for the product.
+   product data is in a data file in the JSON format
+   Print out the product name, price, and quantity if the product is found.
+   if no product matches the search, state that no product was found and start over (let the suffering continue)
+     */
+    public static void main(String[] args){
+        parseDataSet();
+    }
+    public static void parseDataSet(){
+        
+    }
+}
