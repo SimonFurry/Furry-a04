3b0fc552f0af89e793a9431a7eba39e8185d8f78
Simon <simonfurry@gmail,.com>
2021-10-16 12:03:41 -0400

    Skeleton of exercise43

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,21 @@
+package baseline;
+/*
+ * UCF COP 3330 Fall 2021 Assignment 4 Solutions
+ * Copyright 2021 Simon Furry
+ */
+
+public class Solution43 {
+    /*
+    prompt fo the user for a site name
+    prompt for the author of the site
+    ask if the user wants a folder for the JavaScript files
+    ask if the user wants a folder for CSS files
+    generate an index.html fle that contains the name of the site inside the <title> tag and author in a <meta> tag
+     */
+    public static void main(String[] args){
+        generateWebsiteSkeleton();
+    }
+    public static void generateWebsiteSkeleton(){
+
+    }
+}
