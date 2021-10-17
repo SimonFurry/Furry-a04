c8dae8f4ac9b46ae867a662fe5335de72c805eb8
Simon <simonfurry@gmail,.com>
2021-10-17 12:45:31 -0400

    Skeleton of exercise46

---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,25 @@
+package baseline;
+/*
+ * UCF COP 3330 Fall 2021 Assignment 4 Solutions
+ * Copyright 2021 Simon Furry
+ */
+
+public class Solution46 {
+    /*
+    Read in exercise46_input.txt
+    count frequency of the words in the file
+    print from largest to smallest
+     */
+    public static void main(String[] args){
+        frequencyCounter();
+    }
+    public static void frequencyCounter(){
+        /*
+        read from Path path = Paths.get("exercise46_input.txt")
+        create map to track freq
+        while != null
+        continue counting, reserve value
+        valueChart = ("*" * value)
+         */
+    }
+}
