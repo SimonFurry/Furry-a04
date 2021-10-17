86fe6359d21cee89fc8c9fa36546d5d05b198a57
Simon <simonfurry@gmail,.com>
2021-10-17 13:05:48 -0400

    Coding for exercise46

--- exercise46/src/main/java/baseline/Solution46.java
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -4,6 +4,14 @@ package baseline;
  * Copyright 2021 Simon Furry
  */
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Solution46 {
     /*
     Read in exercise46_input.txt
@@ -21,5 +29,33 @@ public class Solution46 {
         continue counting, reserve value
         valueChart = ("*" * value)
          */
+        Path path = Paths.get("exercise46_input.txt");
+        try {
+            String text = Files.readString(path);
+            text = text.toLowerCase(); //convert all to lowercase to match properly
+            Pattern pattern = Pattern.compile("[a-z]+");
+            Matcher matcher = pattern.matcher(text);
+            TreeMap<String, Integer> freq = new TreeMap<>();
+            int frequency = 0;
+            while (matcher.find()) {
+                String word = matcher.group();
+                int letters = word.length();
+                if (letters > frequency) {
+                    frequency = letters;
+                }
+                if (freq.containsKey(word)) {
+                    freq.computeIfPresent(word, (w, c) -> Integer.valueOf(c.intValue() + 1)); //if word is there, increment to the counter for word
+                }
+                else {
+                    freq.computeIfAbsent(word, (w) -> Integer.valueOf(1));
+                }
+            }
+            String format = "%-" + frequency + "s: %2d%n";
+            freq.forEach((k, v) -> System.out.printf(format, k, v));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
