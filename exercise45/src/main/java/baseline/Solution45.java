45f0b47b2b2b309ef9214b33f1f9d7928f050f80
Simon <simonfurry@gmail,.com>
2021-10-17 12:02:21 -0400

    Skeleton of exercise45

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,26 @@
+package baseline;
+/*
+ * UCF COP 3330 Fall 2021 Assignment 4 Solutions
+ * Copyright 2021 Simon Furry
+ */
+
+public class Solution45 {
+    /*
+    Given an input file "exercise45_input.txt":
+    read the file and look for all occurrences of the word utilize
+    find and replace all occurrences with use
+    write the modified file to a new file.
+
+    prompt for a name of the output file,
+    write output to a new file.
+     */
+    public static void main(String[] args){
+        findReplace();
+    }
+    public static void findReplace(){
+        /*
+        try-> path.get("exercise44_input.txt")
+        .replaceAll("utilize, use");
+         */
+    }
+}
